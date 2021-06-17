e488bb00b02b4779fa475f1eee381bacc8e0d94c
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-06-17 11:59:57 +0200

    win condition

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 let doorImage1 = document.getElementById('door1');
 let doorImage2 = document.getElementById('door2');
 let doorImage3 = document.getElementById('door3');
+let startButton = document.getElementById('start');
 
 let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/robot.svg";
 let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 
+let closedDoorPath = "https://content.codecademy.com/projects/chore-door/images/closed_door.svg"; 
+
 let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
+
+const isClicked = (door) => {
+    return door.src != closedDoorPath;
+}
+
+
+
+const playDoor = () => {
+    numClosedDoors--;
+    startButton.innerHTML = numClosedDoors;
+    if (numClosedDoors == 0) gameOver('win');
+}
 
 
 const randomChoreDoorGenerator = () => {
@@ -34,21 +49,37 @@ const randomChoreDoorGenerator = () => {
     }
 }
 
-
-let start = document.getElementById('start');
-
 doorImage1.onclick = () => {
-    doorImage1.src = openDoor1;
-    
+    if(!isClicked(doorImage1))
+    {
+        doorImage1.src = openDoor1;
+        playDoor(); 
+    }  
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = openDoor2;
+    if(!isClicked(doorImage2))
+    {
+        doorImage2.src = openDoor2;
+        playDoor(); 
+    }  
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = openDoor3;
+    if(!isClicked(doorImage3))
+    {
+        doorImage3.src = openDoor3;
+        playDoor(); 
+    }   
 }
+
+const gameOver = (status) => {
+    if (status == 'win') {
+        startButton.innerHTML = 'You win! Play again?'
+    }
+}
+
+
 
 randomChoreDoorGenerator();
 
