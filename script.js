6c3750acb592a593db9387bfd9ad9da32f9d07da
Rody van der Jagt <rodyvanderj8@gmail.com>
2021-06-17 11:48:00 +0200

    Randomize

--- script.js
+++ script.js
@@ -6,14 +6,49 @@ let botDoorPath = "https://content.codecademy.com/projects/chore-door/images/rob
 let beachDoorPath = "https://content.codecademy.com/projects/chore-door/images/beach.svg";
 let spaceDoorPath = "https://content.codecademy.com/projects/chore-door/images/space.svg";
 
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+
+
+const randomChoreDoorGenerator = () => {
+    let choreDoor = Math.floor(Math.random() * numClosedDoors);
+    switch(choreDoor) {
+        case 0:
+            openDoor1 = botDoorPath;
+            openDoor2 = beachDoorPath;
+            openDoor3 = spaceDoorPath;
+            break;
+        case 1:
+            openDoor2 = botDoorPath;
+            openDoor3 = beachDoorPath;
+            openDoor1 = spaceDoorPath;
+            break;
+        case 2:
+            openDoor3 = botDoorPath;
+            openDoor1 = beachDoorPath;
+            openDoor2 = spaceDoorPath;
+            break;  
+        default: break;
+    }
+}
+
+
+let start = document.getElementById('start');
+
 doorImage1.onclick = () => {
-    doorImage1.src = botDoorPath;
+    doorImage1.src = openDoor1;
+    
 }
 
 doorImage2.onclick = () => {
-    doorImage2.src = beachDoorPath;
+    doorImage2.src = openDoor2;
 }
 
 doorImage3.onclick = () => {
-    doorImage3.src = spaceDoorPath;
+    doorImage3.src = openDoor3;
 }
+
+randomChoreDoorGenerator();
+
